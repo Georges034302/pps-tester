06898d7c95d3ee942b33875838a6f633ae1d306c
Georges034302 <georges.boughantous@gmail.com>
2024-12-20 21:59:09 +1100

    test89

--- .github/scripts/update_readme.sh
+++ .github/scripts/update_readme.sh
@@ -5,14 +5,9 @@ FREQ_RESULT=$1
 GITHUB_USER=$2
 TIMESTAMP=$(date)
 
-# Ensure inputs are not empty
-if [[ -z "$FREQ_RESULT" || -z "$GITHUB_USER" ]]; then
-  echo "Error: Missing frequency result or GitHub user."
-  exit 1
-fi
-
 # Update README
-echo "[$GITHUB_USER - $FREQ_RESULT - $TIMESTAMP]" >> README.md
+echo -e "\n[$GITHUB_USER - $FREQ_RESULT - $TIMESTAMP]" >> README.md
+
 # Configure Git to use GitHub Actions user and email
 git config --global user.name "github-actions"
 git config --global user.email "github-actions@users.noreply.github.com"
